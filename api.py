a865059b58b174c179c52f37c54b5c95c0ebae74
glevin7523 <glevinroche@gmail.com>
2025-03-11 16:08:21 +0530

    Delete api.py

--- api.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyA2N47LKWm5HSEf2HK-cJ0ErwNW2d4EFqE")
-
-models = genai.list_models()
-for model in models:
-    print(model.name)
